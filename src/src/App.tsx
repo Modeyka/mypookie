32466ebe42a339052695bbb9f1d5aa1214ef81b6
Modeyka <modeykulla@gmail.com>
2025-06-05 03:55:45 +0300

    Create App.tsx

---
+++ src/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-pink-900 flex items-center justify-center text-white font-['Press_Start_2P']">
+      <h1 className="text-4xl">Interactive Pookie Fun!</h1>
+    </div>
+  );
+};
+
+export default App;
